cfb8129c513507c8ee183094cea60e2cae7a3f58
PabloFP64 <63953769+PabloFP64@users.noreply.github.com>
2023-02-09 18:49:31 +0100

    hecho el ejercicio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,57 @@
-package org.example;public class Main {
+package org.example;
+
+interface Dialogo {
+    void render();
+}
+
+class DialogoWindows implements Dialogo {
+    @Override
+    public void render() {
+        System.out.println("Rendering del diálogo de Windows");
+    }
+}
+
+class DialogoMac implements Dialogo {
+    @Override
+    public void render() {
+        System.out.println("Rendering del diálogo de Mac");
+    }
+}
+
+interface FabricaDialogo {
+    Dialogo crearDialogo();
+}
+
+class FabricaDialogoWindows implements FabricaDialogo {
+    @Override
+    public Dialogo crearDialogo() {
+        return new DialogoWindows();
+    }
+}
+
+class FabricaDialogoMac implements FabricaDialogo {
+    @Override
+    public Dialogo crearDialogo() {
+        return new DialogoMac();
+    }
+}
+
+class Cliente {
+    private Dialogo dialogo;
+
+    public Cliente(FabricaDialogo fabrica) {
+        dialogo = fabrica.crearDialogo();
+    }
+
+    public void render() {
+        dialogo.render();
+    }
+}
+
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FabricaDialogo fabrica = new FabricaDialogoWindows();
+        Cliente cliente = new Cliente(fabrica);
+        cliente.render();
     }
 }
